bfcbcad684ec625d3190f2cc5f9cc082e586d00c
Sarah Nosal <nosalsm@gmail.com>
2021-11-11 10:25:56 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let i=0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
+
+
